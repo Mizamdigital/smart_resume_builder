edb33e17bbae4f9b4d3698d36baefebd09f69e93
Mizamdigital <ayoksbaby4real@yahoo.com>
2026-05-24 06:33:46 +0100

    Initial project structure

---
+++ config/database.php
@@ -0,0 +1,23 @@
+<?php
+
+$host = 'localhost';
+$dbname = 'smart_resume_builder';
+$username = 'root';
+$password = '';
+
+
+try{
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname",
+        $username,
+        $password
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+
+
+?>
